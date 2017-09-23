5b99bc94e2a301a9f65d9baee571c9ede9a35898
burpingdog1 <bdogsproduction@gmail.com>
2017-09-22 20:47:07 -0500

    File fixes & addition of Tile item
    carpenters block still doesnt render

--- src/main/java/com/carpentersblocks/util/registry/ItemRegistry.java
+++ src/main/java/com/carpentersblocks/util/registry/ItemRegistry.java
@@ -3,18 +3,19 @@ package com.carpentersblocks.util.registry;
 import com.carpentersblocks.CarpentersBlocks;
 import com.carpentersblocks.item.ItemCarpentersChisel;
 import com.carpentersblocks.item.ItemCarpentersHammer;
-
+import com.carpentersblocks.item.ItemCarpentersTile;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import static com.carpentersblocks.Reference.*;
+
 public class ItemRegistry 
 { 
     public static Item itemCarpentersHammer;
@@ -23,13 +24,8 @@ public class ItemRegistry
     public static Item itemCarpentersBed;
     public static Item itemCarpentersTile;   
     
-    /**
-     * Registers item IDs.
-     */
     public static void preInit(FMLPreInitializationEvent event)
-    {  
-        // Register items
-        
+    {      
         if (enableHammer)
         {
             itemCarpentersHammer = new ItemCarpentersHammer()
@@ -38,15 +34,18 @@ public class ItemRegistry
             		.setCreativeTab(CarpentersBlocks.CREATIVE_TAB)
             		.setMaxStackSize(1);
             GameRegistry.register(itemCarpentersHammer);
+            
             if (itemCarpentersToolsDamageable)
             {
             	itemCarpentersHammer.setMaxDamage(itemCarpentersToolsUses);
             }
+            
             if (Side.CLIENT.equals(event.getSide()))
             {
             	ModelLoader.setCustomModelResourceLocation(itemCarpentersHammer, 0, new ModelResourceLocation(MOD_ID + ":" + "carpenters_hammer"));
             }
         }
+        
         if (enableChisel) 
         {
             itemCarpentersChisel = new ItemCarpentersChisel()
@@ -55,28 +54,30 @@ public class ItemRegistry
             		.setCreativeTab(CarpentersBlocks.CREATIVE_TAB)
             		.setMaxStackSize(1);
             GameRegistry.register(itemCarpentersChisel);
+            
             if (itemCarpentersToolsDamageable)
             {
             	itemCarpentersChisel.setMaxDamage(itemCarpentersToolsUses);
             }
+            
             if (Side.CLIENT.equals(event.getSide()))
             {
             	ModelLoader.setCustomModelResourceLocation(itemCarpentersChisel, 0, new ModelResourceLocation(MOD_ID + ":" + "carpenters_chisel"));
             }
         }
-        /*
-        if (enableTile) {
-            itemCarpentersTile = new ItemCarpentersTile().setUnlocalizedName("itemCarpentersTile");
-            GameRegistry.registerItem(itemCarpentersTile, "itemCarpentersTile");
+        
+        if (enableTile) 
+        {
+            itemCarpentersTile = new ItemCarpentersTile()
+            		.setUnlocalizedName("itemCarpentersTile")
+            		.setRegistryName("carpenters_tile");
+            GameRegistry.register(itemCarpentersTile);
+            
+            if (Side.CLIENT.equals(event.getSide()))
+            {
+            	ModelLoader.setCustomModelResourceLocation(itemCarpentersTile, 0, new ModelResourceLocation(MOD_ID + ":" + "carpenters_tile"));
+            }
         }
-        if (BlockRegistry.enableDoor) {
-            itemCarpentersDoor = new ItemCarpentersDoor().setUnlocalizedName("itemCarpentersDoor");
-            GameRegistry.registerItem(itemCarpentersDoor, "itemCarpentersDoor");
-        }
-        if (BlockRegistry.enableBed) {
-            itemCarpentersBed = new ItemCarpentersBed().setUnlocalizedName("itemCarpentersBed");
-            GameRegistry.registerItem(itemCarpentersBed, "itemCarpentersBed");
-        }*/
     }
 
     public static void init(FMLInitializationEvent event)
@@ -85,18 +86,15 @@ public class ItemRegistry
         {
             GameRegistry.addRecipe(new ItemStack(itemCarpentersHammer, 1), new Object[] { "XX ", " YX", " Y ", 'X', Items.IRON_INGOT, 'Y', BlockRegistry.blockCarpentersBlock });
         }
+        
         if (enableChisel) 
         {
             GameRegistry.addRecipe(new ItemStack(itemCarpentersChisel, 1), new Object[] { "X", "Y", 'X', Items.IRON_INGOT, 'Y', BlockRegistry.blockCarpentersBlock });
         }
-        /*        if (enableTile) {
-            GameRegistry.addRecipe(new ItemStack(itemCarpentersTile, recipeQuantityTile), new Object[] { "XXX", "YYY", 'X', Blocks.hardened_clay, 'Y', BlockRegistry.blockCarpentersBlock });
+        
+        if (enableTile) 
+        {
+            GameRegistry.addRecipe(new ItemStack(itemCarpentersTile, recipeQuantityTile), new Object[] { "XXX", "YYY", 'X', Blocks.HARDENED_CLAY, 'Y', BlockRegistry.blockCarpentersBlock });
         }
-        if (BlockRegistry.enableDoor) {
-            GameRegistry.addRecipe(new ItemStack(itemCarpentersDoor, BlockRegistry.recipeQuantityDoor), new Object[] { "XX", "XX", "XX", 'X', BlockRegistry.blockCarpentersBlock });
-        }
-        if (BlockRegistry.enableBed) {
-            GameRegistry.addRecipe(new ItemStack(itemCarpentersBed, BlockRegistry.recipeQuantityBed), new Object[] { "XXX", "YYY", 'X', Blocks.wool, 'Y', BlockRegistry.blockCarpentersBlock });
-        }*/
     }  
 }
